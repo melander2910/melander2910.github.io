ebaa171656c3de323f37507d98a3fe50cb16fe87
melander2910 <58288414+melander2910@users.noreply.github.com>
2021-06-19 12:26:09 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+const theme = document.getElementById("theme");
+
+
+
+theme.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    //document.getElementsByClassName("drumContainer").classList.toggle("dark")
+});
+
